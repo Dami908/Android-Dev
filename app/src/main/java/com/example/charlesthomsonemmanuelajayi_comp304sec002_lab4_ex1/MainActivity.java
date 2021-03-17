b17ed3e9a0226dfd3c12647608b3369838cc4b37
Dami908 <55024367+Dami908@users.noreply.github.com>
2021-03-17 19:31:36 -0400

    Activity/Navigation Setup

--- app/src/main/java/com/example/charlesthomsonemmanuelajayi_comp304sec002_lab4_ex1/MainActivity.java
+++ app/src/main/java/com/example/charlesthomsonemmanuelajayi_comp304sec002_lab4_ex1/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.charlesthomsonemmanuelajayi_comp304sec002_lab4_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menubar_activity,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent intent;
+        switch(item.getItemId()){
+            case R.id.id_patient:
+                intent =new Intent(this,patientActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.id_testActivity:
+                intent =new Intent(this,testActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.id_updateInfo:
+                intent =new Intent(this,updateInfoActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.id_viewTest:
+                intent =new Intent(this,viewActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
